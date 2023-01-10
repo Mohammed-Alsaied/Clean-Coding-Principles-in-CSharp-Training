c9ea5a3d0950c9efb13368736a1139b81eb6661b
Mohamed Alsaied <mohamed.saied.civil@gmail.com>
2023-01-10 19:45:25 +0200

    10.Staying Clean

---
+++ 10.Staying Clean/10.Staying Clean.cs
@@ -0,0 +1,70 @@
+
+#region Agenda
+/*
+ * The Path Forward:
+ *	When to refactor
+ *	Techneiques for staying clean
+ */
+#endregion Agenda
+
+#region When to Refactor to Clean Code 
+/*
+ * You need to work with the code 
+ * Difficult to comprehend or change 
+ * Add tests for regression protection 
+ */
+#endregion When to Refactor to Clean Code 
+
+#region Code Reviews 
+/*
+ * Promote proactive cleanliness 
+ * Set guidelines 
+ * Assure readability 
+ */
+#endregion Code Reviews
+
+#region Work in Pairs  
+/*
+ * Real-time code review 
+ * Increases quality  
+ * Naming and refactoring is easier  
+ */
+#endregion Code Reviews
+
+#region Host Lunch & Learns 
+/*
+ * Showcase what you've learned 
+ * Be a thought leader 
+ * Spark interest in others 
+ * Get free food! 
+ */
+#endregion Host Lunch & Learns 
+
+#region Accept No Broken Windows 
+/*
+ * A building with a few broken windows is quickly disrespected.
+ * A disrespected code base can have the same issues.
+ * The solutions:
+ * Starting today,we arenot going to accept more "broken windows" in our code.
+ */
+#endregion Accept No Broken Windows 
+
+#region The Boy Scout Rule 
+/*
+ * "Leave the code you're editing a little better than you found it." 
+	Robert C. Martin 
+ */
+#endregion The Boy Scout Rule 
+
+#region Summary
+/*
+ * Refactor when necessary 
+ * Add tests first for protection 
+ * Set and maintain standards 
+ * Do code reviews 
+ * Pair program 
+ * Host lunch and learns 
+ * Watch for broken windows 
+ * Leave the code better than you found it 
+ */
+#endregion Summary
